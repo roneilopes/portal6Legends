e735bd734831dbc7278a1c2d392522b07c9bb722
AlineOliveira <aliine.belle20@gmail.com>
2016-05-06 11:30:51 -0400

    Aline Contato

--- index.php
+++ index.php
@@ -6,19 +6,12 @@ Edição da Aline
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <style>
-            p {
-                border: 1px solid red;
-                background-color: yellow;
-                padding: 50px 30px 50px 80px;
-            }
-        </style>
+        
     </head>
     <body>
         <h2>Os Dev's</h2>
         <p>Pandas</p>;
         
-
     </body>
 </head>
 <body>
