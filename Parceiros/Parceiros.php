2b6e27ed91d512cfab851b5e612845c09a9efb81
MarceloSobrinho <sobrinho11191@gmail.com>
2016-05-06 23:08:46 -0400

    Add

--- Parceiros/Parceiros.php
+++ Parceiros/Parceiros.php
@@ -9,22 +9,63 @@
             body {
                 background-color: whitesmoke;
             }
+            #titulo {
+                background-color:#999999;
+                color:white;
+                text-align:center;
+                padding:5px;
+            }
+            #rodape {
+                background-color:#999999;
+                color:white;
+                clear:both;
+                text-align:center;
+                padding:5px;	 	 
+            }
+            #direita{
+                line-height:30px;
+                background-color:#eeeeee;
+                height:470px;
+                width:130px;
+                float:left;
+                padding:5px;	      
+            }
         </style>
 
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div>
+        <div id="titulo">
             <h2>PARCEIROS</h2>
-            <img src="Logo/ALCAIDA.PNG" alt=""/>
-            <img src="Logo/ERRO.PNG" alt=""/>
-            <img src="Logo/FACÇÃO.PNG" alt=""/>
-            <img src="Logo/KARADA.PNG" alt=""/>
-            <img src="Logo/LADIES.PNG" alt=""/>
-            <img src="Logo/R-E-P.PNG" alt=""/>
-            <img src="Logo/TAES-COMPANY.PNG" alt=""/>
-            <img src="Logo/XABLAU.PNG" alt=""/>
+        </div>
+        <div id="direita">
+            <n> <p>ALCAIRA</p> 
+                <p>ERRO-404</p> 
+                <p>FACÇÃO</p> 
+                <p>LADIES</p> 
+                <p>R-E-P</p> 
+                <p>TAES-COMPANY</p> 
+                <p>XABLAU</p> 
+                <p>KARDASHIANS</p> 
+            </n>
+        </div>
+        <div >
+            <center>
+               <br><br>
+                <a href="http://migre.me/tI1lX"><img src="Logo/Alcalda.PNG" alt=""/></a> 
+                <a href="http://migre.me/tI1kK"><img src="Logo/ERRO.PNG" alt=""/></a>
+                <a href="http://migre.me/tI1nJ"><img src="Logo/FACÇÃO.PNG" alt=""/></a>
+                <a href="http://migre.me/tI1sC"><img src="Logo/LADIES.PNG" alt=""/></a>
+                <a href="http://migre.me/tI1tG"><img src="Logo/R-E-P.PNG" alt=""/></a>
+                <a href="http://migre.me/tI1z4"><img src="Logo/TAES-COMPANY.PNG" alt=""/></a>
+                <a href="http://migre.me/tI1A0"><img src="Logo/XABLAU.PNG" alt=""/></a><br><br>
+                <a href="http://migre.me/tI1BD"><img src="Logo/KARDASHIANS.PNG" alt=""/></a>
+
+            </center>
+        </div>
+        <div id="rodape">
+            <h3> © 6Lengends </h3>
         </div>
     </body>
 </html>
