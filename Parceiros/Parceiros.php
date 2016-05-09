4f71a55799e983e69cc641248d207f2b075848aa
MarceloSobrinho <sobrinho11191@gmail.com>
2016-05-09 19:00:45 -0400

    mudaça de link redusido

--- Parceiros/Parceiros.php
+++ Parceiros/Parceiros.php
@@ -53,14 +53,14 @@
         <div >
             <center>
                <br><br>
-               <a href="http://migre.me/tI1lX"><img src="Logo/Alcaida.PNG" title="Veja o que o nosso parceiro Alcaida está trabalhando"/></a> 
-                <a href="http://migre.me/tI1kK"><img src="Logo/ERRO.PNG" title="Veja o que o nosso parceiro ERRO-404 está trabalhando"/></a>
-                <a href="http://migre.me/tI1nJ"><img src="Logo/FACÇÃO.PNG" title="Veja o que o nosso parceiro FACÇÃO está trabalhando"/></a>
-                <a href="http://migre.me/tI1sC"><img src="Logo/LADIES.PNG" title="Veja o que o nosso parceiro LADIES está trabalhando"/></a>
-                <a href="http://migre.me/tI1tG"><img src="Logo/R-E-P.PNG" title="Veja o que o nosso parceiro R-E-P está trabalhando"/></a>
-                <a href="http://migre.me/tI1z4"><img src="Logo/TAES-COMPANY.PNG" title="Veja o que o nosso parceiro TAES-COMPANY está trabalhando"/></a>
-                <a href="http://migre.me/tI1A0"><img src="Logo/XABLAU.PNG" title="Veja o que o nosso parceiro XABLAU está trabalhando"/></a><br><br>
-                <a href="http://migre.me/tI1BD"><img src="Logo/KARDASHIANS.PNG"title="Veja o que o nosso parceiro KARDASHIANS está trabalhando"/></a>
+               <a href="https://drive.google.com/folderview?id=0BxRVql20HumldVZ3LTlwRGxlT2c&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/Alcaida.PNG" title="Veja o que o nosso parceiro Alcaida está trabalhando"/></a> 
+                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN09SekkycmExLTg&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/ERRO.PNG" title="Veja o que o nosso parceiro ERRO-404 está trabalhando"/></a>
+                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlQ19YUm1TUGU5VlE&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/FACÇÃO.PNG" title="Veja o que o nosso parceiro FACÇÃO está trabalhando"/></a>
+                <a href="https://drive.google.com/folderview?id=0BxRVql20Humla2Q4TUlCLUlocm8&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/LADIES.PNG" title="Veja o que o nosso parceiro LADIES está trabalhando"/></a>
+                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlRkVHRWpyQzZ6QWM&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/R-E-P.PNG" title="Veja o que o nosso parceiro R-E-P está trabalhando"/></a>
+                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlWEZHZE5SbHZQZ0k&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/TAES-COMPANY.PNG" title="Veja o que o nosso parceiro TAES-COMPANY está trabalhando"/></a>
+                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlcG9WTFBVWVgwS3M&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/XABLAU.PNG" title="Veja o que o nosso parceiro XABLAU está trabalhando"/></a><br><br>
+                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN0xyN1l4ZVNGdGs&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/KARDASHIANS.PNG"title="Veja o que o nosso parceiro KARDASHIANS está trabalhando"/></a>
 
             </center>
         </div>
