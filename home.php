70c424bf58e3c7a56a076efb3f262bd5b2f7fd64
Ronei Lopes <roneilopis@gmail.com>
2016-05-11 11:35:49 -0400

    Adicionando a Página Principal

--- home.php
+++ home.php
@@ -5,24 +5,61 @@ Edição Ronei
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-        
+        <title>6Legends</title>
+        <style>
+            body {
+                background-color: whitesmoke;
+            }
+            #logotipo {
+                background-color: #999999;
+                color:white;
+                text-align: left;
+                padding:5px;
+            }
+            #Desc{
+                position: absolute;
+                top: 120px;                
+                right: 400px;
+                padding: 10px;
+            }
+            p {
+                font-family: "Times New Roman", Times, serif;
+            }
+            a{
+                font-size: 22px;
+                font-family: "Times New Roman", Times, serif;;
+                font-style: italic;
+            }
+            a:link {
+                color: black;
+            }
+
+            /* mouse over link */
+            a:hover {
+                color: blue;
+            }
+
+            /* selected link */
+            a:active {
+                color: green;
+            }
+        </style>
     </head>
     <body>
-        <h2>Descrição da Equipe</h2>
-        <?php
-        // put your code here
-        ?>
+        <h1 align="center">Portal 6Legends</h1><br/><br/>
+            <div id="logotipo">
+                <img src="logo.png" alt="Logo" style="width: 35%;"/>
+                <div id="Desc">
+                    <h2>O time 6Legends</h2>
+                </div>
+            </div>
+        <div style="width: 70%; margin-left: 10%;">            
+            <a href="ApresentacaoTecnologias/apresentaçaoTecnologias.html"><h3>Tecnologias Utilizadas</h3></a>
+            <a href="produto_Jeferson.php"><h3>Produtos</h3></a>
+            <a href="Parceiros/Parceiros.php"><h3>Parceiros</h3></a>
+            <a href="contato.php"><h3>Contate-nos</h3></a>                        
+        </div>
+        <div align="center">&#169; 2016 6Legends</div>
     </body>
-<<<<<<< HEAD
 </head>
-<body>
-
-    <h1>Nosso Produto</h1>
-    
-
-</body>
-
-=======
->>>>>>> 75e6e22d83be455c8934464adf9f798950d3b4bd
 </html>
