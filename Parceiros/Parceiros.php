9444f019809e5088df3f62f523a2987352f1c435
MarceloSobrinho <sobrinho11191@gmail.com>
2016-05-12 12:10:39 -0400

    correção nos parceiros rodapé e titulo

--- Parceiros/Parceiros.php
+++ Parceiros/Parceiros.php
@@ -14,6 +14,8 @@
                 color:white;
                 text-align:center;
                 padding:5px;
+                position: fixed;
+                width:98%;
             }
             #rodape {
                 background-color:#999999;
@@ -21,15 +23,21 @@
                 clear:both;
                 text-align:center;
                 padding:5px;
-                position: fixid;
+               
             }
             #direita{
                 line-height:30px;
                 background-color:#eeeeee;
-                height:470px;
-                width:130px;
+                height:570px;
+                width:15%;
                 float:left;
-                padding:5px;	      
+                padding:5px;
+                
+ 
+                
+            }
+            p{
+              vertical-align: central;  
             }
         </style>
 
@@ -40,31 +48,35 @@
         <div id="titulo">
             <h2>PARCEIROS</h2>
         </div>
+        <br><br><br><br>
         <div id="direita">
+         <p>
             <n><a href="https://drive.google.com/folderview?id=0BxRVql20HumldVZ3LTlwRGxlT2c&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"> <p>ALCAIDA</p></a>
                 <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN09SekkycmExLTg&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><p>ERRO-404</p></a> 
                 <a href="https://drive.google.com/folderview?id=0BxRVql20HumlQ19YUm1TUGU5VlE&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><p>FACÇÃO</p></a> 
                 <a href="https://drive.google.com/folderview?id=0BxRVql20Humla2Q4TUlCLUlocm8&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><p>LADIES</p></a> 
                 <a href="https://drive.google.com/folderview?id=0BxRVql20HumlRkVHRWpyQzZ6QWM&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><p>R-E-P</p></a> 
                 <a href="https://drive.google.com/folderview?id=0BxRVql20HumlWEZHZE5SbHZQZ0k&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><p>TAES-COMPANY</p></a>
-               <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN0xyN1l4ZVNGdGs&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><p>KARDASHIANS</p></a>
+                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN0xyN1l4ZVNGdGs&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><p>KARDASHIANS</p></a>
             </n>
-        </div>
-        <div >
-            <center>
-               <br><br>
-               <a href="https://drive.google.com/folderview?id=0BxRVql20HumldVZ3LTlwRGxlT2c&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/Alcaida.PNG" title="Veja o que o nosso parceiro Alcaida está trabalhando"/></a> 
-                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN09SekkycmExLTg&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/ERRO.PNG" title="Veja o que o nosso parceiro ERRO-404 está trabalhando"/></a>
-                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlQ19YUm1TUGU5VlE&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/FACÇÃO.PNG" title="Veja o que o nosso parceiro FACÇÃO está trabalhando"/></a>
-                <a href="https://drive.google.com/folderview?id=0BxRVql20Humla2Q4TUlCLUlocm8&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/LADIES.PNG" title="Veja o que o nosso parceiro LADIES está trabalhando"/></a>
-                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlRkVHRWpyQzZ6QWM&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/R-E-P.PNG" title="Veja o que o nosso parceiro R-E-P está trabalhando"/></a>
-                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlWEZHZE5SbHZQZ0k&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/TAES-COMPANY.PNG" title="Veja o que o nosso parceiro TAES-COMPANY está trabalhando"/></a><br><br>
-                <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN0xyN1l4ZVNGdGs&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/KARDASHIANS.PNG"title="Veja o que o nosso parceiro KARDASHIANS está trabalhando"/></a>
+         </p>
+    </div>
+    <div >
+        <center>
+            <br><br>
+            <a href="https://drive.google.com/folderview?id=0BxRVql20HumldVZ3LTlwRGxlT2c&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/Alcaida.PNG" title="Veja o que o nosso parceiro Alcaida está trabalhando"/></a> 
+            <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN09SekkycmExLTg&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/ERRO.PNG" title="Veja o que o nosso parceiro ERRO-404 está trabalhando"/></a>
+            <a href="https://drive.google.com/folderview?id=0BxRVql20HumlQ19YUm1TUGU5VlE&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/FACÇÃO.PNG" title="Veja o que o nosso parceiro FACÇÃO está trabalhando"/></a>
+            <a href="https://drive.google.com/folderview?id=0BxRVql20Humla2Q4TUlCLUlocm8&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/LADIES.PNG" title="Veja o que o nosso parceiro LADIES está trabalhando"/></a>
+            <a href="https://drive.google.com/folderview?id=0BxRVql20HumlRkVHRWpyQzZ6QWM&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/R-E-P.PNG" title="Veja o que o nosso parceiro R-E-P está trabalhando"/></a>
+            <a href="https://drive.google.com/folderview?id=0BxRVql20HumlWEZHZE5SbHZQZ0k&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/TAES-COMPANY.PNG" title="Veja o que o nosso parceiro TAES-COMPANY está trabalhando"/></a><br><br>
+            <a href="https://drive.google.com/folderview?id=0BxRVql20HumlN0xyN1l4ZVNGdGs&usp=sharing&tid=0BxRVql20HumlbktiVnhFbXFEalU"><img src="Logo/KARDASHIANS.PNG"title="Veja o que o nosso parceiro KARDASHIANS está trabalhando"/></a>
 
-            </center>
-        </div>
-        <div id="rodape">
-            <h3> 2016 ©6Legends </h3>
-        </div>
-    </body>
+        </center>
+    </div>
+    <br><br><br><br><br><br><br>
+    <div id="rodape">
+        <h3> 2016 ©6Legends </h3>
+    </div>
+</body>
 </html>
