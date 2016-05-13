db17b9b91945bc82212f0435c907693015bfe103
LUCAS BOHRER DE SOUZA <08435701646@ifro.local>
2016-05-13 10:50:48 -0400

    alteração feita pela Aline na conta do Lucas

--- contato.php
+++ contato.php
@@ -9,9 +9,14 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Fale Conosco</title>
         <style>
-            input {
+            #cx {
                 height: 30px;
                 border-style: hidden;
+                align-items: center;
+            }
+            #bt {
+                height: 30px;
+                border-style:groove;
                 align-items: center;
             }
             body {
@@ -44,27 +49,27 @@ and open the template in the editor.
             
             <p></p>
             <label name="nome">Nome: </label>
-            <input type="text" name="nome" size="50px" >
+            <input type="text" name="nome" id="cx"size="50px" >
             <br>
             <br>
             <label name="email">E-mail: </label>
-            <input type="text" name="email" size="50px">
+            <input type="text" name="email" id="cx" size="50px">
             <br>
             <br>
             <label name="cidade">Cidade: </label>
-            <input type="text" name="cidade" size="50px">
+            <input type="text" name="cidade" id="cx"size="50px">
             <br>
             <br>
 
             <label name="assunto">Assunto: </label>
-            <input type="text" name="assunto" size="50px">
+            <input type="text" id="cx" name="assunto" size="50px">
             <br>
             <br>
             <label name="descricao">Descrição: </label>
-            <input type="text" name="descricao" size="50px">
+            <input type="text" name="descricao" id="cx" size="50px">
             <br>
             <br>
-            <input type="submit" value="Enviar">
+            <input type="submit" id="bt" value="Enviar">
             <div id="rodape">
                 <h3> 2016 ©6Legends </h3>
             </div>
